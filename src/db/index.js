88eda9ab384b31451d00a39a5999a9df61a8026b
praveen kumar <praveenkumar@praveens-MacBook-Air.local>
2025-11-27 14:23:50 +0530

    implement multer middleware

--- src/db/index.js
+++ src/db/index.js
@@ -1,13 +1,10 @@
 import mongoose, { connect } from 'mongoose';
-import { DB_NAME } from '../constants.js';
 
 const connectDB = async () => {
     try {
-
+        // console.log("Final URI ->", JSON.stringify(process.env.MONGODB_URI));
         const data = await mongoose.connect(`${process.env.MONGODB_URI}`)
         console.log("connected this ")
-
-
     }
     catch (error) {
         console.log("mongodb is not connected", error)
